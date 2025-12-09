84d00f5ec6fafbea98ec822476a82012363320dc
Gene Ipsum <cavinomanu@gmail.com>
2025-12-09 15:42:51 +0100

    adaptation outputs séparés

--- main.py
+++ main.py
@@ -9,26 +9,40 @@ from transcription import (
 
 
 def main(input_file: str) -> None:
+    # Vars
     input_path = Path(input_file)
-    audio_path = Path(f"audio-{input_path.stem}.wav")
-    srt_path = Path(f"sub-{input_path.stem}.en.srt")
-    output_path = Path("output.mp4")
+    base_name = input_path.stem  # ex: "input"
+    audio_dir = Path("output_audio")
+    subs_dir = Path("output_sub")
+    video_dir = Path("output_video")
 
+    # Checks
+    audio_dir.mkdir(exist_ok=True)
+    subs_dir.mkdir(exist_ok=True)
+    video_dir.mkdir(exist_ok=True)
+
+    # Output
+    audio_path = audio_dir / f"{base_name}.wav"
+    srt_path = subs_dir / f"{base_name}.en.srt"
+    output_path = video_dir / f"{base_name}_subtitled.mp4"
+
+    # Pipeline
     print("Chargement du modèle…")
     model = load_whisper_model()
 
-    print("Extraction audio…")
+    print(f"Extraction audio → {audio_path}")
     extract_audio(input_path, audio_path)
 
-    print("Transcription…")
+    print(f"Transcription → {srt_path}")
     transcription = transcribe_audio(audio_path, model)
 
     print("Génération SRT…")
     generate_srt(transcription, srt_path)
 
-    print("Création de la vidéo…")
+    print(f"Création de la vidéo → {output_path}")
     render_video(input_path, srt_path, output_path, embedded=False)
 
+    # Prints cmd
     print("\nTranscription :")
     print(transcription["text"])
 
@@ -41,8 +55,4 @@ if __name__ == "__main__":
         sys.exit(1)
 
     input_file = sys.argv[1]
-
-    input_path = Path(input_file).resolve()
-
     main(input_file)
-
